0c1b89038e518c3b6928b9679aee80900468dd5c
mikaduki <mikaduki.0510@gmail.com>
2014-12-13 16:44:27 +0900

    layout friend, friendlistitem,の編集
     friend
     これからはじめます。
    
     friendlistitem
     friend icon, friend nameを追加

--- app/src/main/java/com/jsnk77/quitsmoking/FriendList.java
+++ app/src/main/java/com/jsnk77/quitsmoking/FriendList.java
@@ -4,4 +4,7 @@ package com.jsnk77.quitsmoking;
  * Created by jsnk77 on 14/12/13.
  */
 public class FriendList {
+    public String id;
+    public String myfbid;
+    public String friendsfbid;
 }
--- app/src/main/java/com/jsnk77/quitsmoking/HomeActivity.java
+++ app/src/main/java/com/jsnk77/quitsmoking/HomeActivity.java
@@ -3,6 +3,13 @@ package com.jsnk77.quitsmoking;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by jsnk77 on 14/12/13.
@@ -10,21 +17,38 @@ import android.os.Bundle;
 public class HomeActivity extends Activity {
 
 
+    @InjectView(R.id.usericon)
+    ImageView mUsericon;
+    @InjectView(R.id.username)
+    TextView mUsername;
+    @InjectView(R.id.Birthday)
+    TextView mBirthday;
+    @InjectView(R.id.gender)
+    TextView mGender;
+    @InjectView(R.id.totalcount)
+    TextView mTotalcount;
+    @InjectView(R.id.imageView)
+    ImageView mImageView;
+    @InjectView(R.id.friendlist)
+    ListView mFriendlist;
+    @InjectView(R.id.option)
+    ImageButton mOption;
+    @InjectView(R.id.imageButton)
+    ImageButton mImageButton;
     private Intent intent;
     private String fbId;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        ButterKnife.inject(this);
 
 
         intent = getIntent();
         fbId = intent.getStringExtra("fbId");
 
-        Intent i = new Intent(this,ShareActivity.class);
+        Intent i = new Intent(this, ShareActivity.class);
         startActivity(i);
-
-
 
 
     }
